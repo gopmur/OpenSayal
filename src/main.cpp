d12826f83d88f8a6dd2ad74ba486266d53abef24
Ali Jafari <ali81jafari81@outlook.com>
2025-04-06 19:20:32 +0330

    feat: added arrows

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
-#include <stdint.h>
+#include <cmath>
+#include <cstdint>
 #include <cstdlib>
 #include <iostream>
 #include <tuple>
@@ -7,6 +8,7 @@
 #include "SDL_error.h"
 #include "SDL_events.h"
 #include "SDL_render.h"
+#include "SDL_timer.h"
 #include "SDL_video.h"
 
 #include "logger.hpp"
@@ -14,6 +16,7 @@
 
 #define HEIGHT 100
 #define WIDTH 100
+#define CELL_SIZE 10
 
 float pixelData[HEIGHT][WIDTH];
 
@@ -23,14 +26,15 @@ void generatePixelData() {
   for (int y = 0; y < HEIGHT; ++y) {
     for (int x = 0; x < WIDTH; ++x) {
       if (x + offset < WIDTH) {
-        pixelData[y][x + offset] = static_cast<float>(x) / WIDTH;  // Gradient from 0 to 1
-      }
-      else {
-        pixelData[y][x + offset - WIDTH] = static_cast<float>(x) / WIDTH;
+        pixelData[y][x + offset] =
+            static_cast<float>(WIDTH - x - 1) / WIDTH;  // Gradient from 0 to 1
+      } else {
+        pixelData[y][x + offset - WIDTH] =
+            static_cast<float>(WIDTH - x - 1) / WIDTH;
       }
     }
   }
-  offset++;
+
   offset %= WIDTH;
 }
 
@@ -63,9 +67,9 @@ std::tuple<SDL_Window*, SDL_Renderer*, SDL_Texture*> setup() {
     exit(EXIT_FAILURE);
   }
 
-  window =
-      SDL_CreateWindow("Fluid simulation", SDL_WINDOWPOS_CENTERED,
-                       SDL_WINDOWPOS_CENTERED, WIDTH * 10, HEIGHT * 10, SDL_WINDOW_SHOWN);
+  window = SDL_CreateWindow("Fluid simulation", SDL_WINDOWPOS_CENTERED,
+                            SDL_WINDOWPOS_CENTERED, WIDTH * CELL_SIZE,
+                            HEIGHT * CELL_SIZE, SDL_WINDOW_SHOWN);
   if (window == nullptr) {
     auto sdl_error_message = SDL_GetError();
     std::cout << "Window creation failed: " << sdl_error_message << std::endl;
@@ -93,17 +97,37 @@ std::tuple<SDL_Window*, SDL_Renderer*, SDL_Texture*> setup() {
   return {window, renderer, texture};
 }
 
-// void main_loop(SDL_Window* window,
-//                SDL_Renderer* renderer,
-//                SDL_Texture* texture) {
-//   SDL_UpdateWindowSurface(window);
-// }
+void draw_arrow(SDL_Renderer* renderer,
+                uint32_t x,
+                uint32_t y,
+                float length,
+                float angle,
+                float arrow_head_length = 5,
+                float arrow_head_angle = M_PI / 6) {
+  int32_t x_offset = length * std::cos(angle);
+  int32_t y_offset = -length * std::sin(angle);
+  uint32_t x2 = x + x_offset;
+  uint32_t y2 = y + y_offset;
+  SDL_RenderDrawLine(renderer, x, y, x2, y2);
+  
+  uint32_t arrow_x_offset = -arrow_head_length * std::cos(angle + arrow_head_angle);
+  uint32_t arrow_y_offset = arrow_head_length * std::sin(angle + arrow_head_angle);
+  uint32_t arrow_x2 = x2 + arrow_x_offset;
+  uint32_t arrow_y2 = y2 + arrow_y_offset;
+  SDL_RenderDrawLine(renderer, x2, y2, arrow_x2, arrow_y2);
+  
+  arrow_x_offset = -arrow_head_length * std::cos(arrow_head_angle - angle);
+  arrow_y_offset = -arrow_head_length * std::sin(arrow_head_angle - angle);
+  arrow_x2 = x2 + arrow_x_offset;
+  arrow_y2 = y2 + arrow_y_offset;
+  SDL_RenderDrawLine(renderer, x2, y2, arrow_x2, arrow_y2);
+}
 
 int main() {
   auto [window, renderer, texture] = setup();
   Logger::static_log("Window created successfully");
 
-  Uint32* pixels = new Uint32[WIDTH * HEIGHT];
+  uint32_t* pixels = new uint32_t[WIDTH * HEIGHT];
   SDL_PixelFormat* format = SDL_AllocFormat(SDL_PIXELFORMAT_RGBA8888);
 
   SDL_Event event;
@@ -117,20 +141,23 @@ int main() {
     generatePixelData();
     for (int y = 0; y < HEIGHT; ++y) {
       for (int x = 0; x < WIDTH; ++x) {
-        Uint8 color =
-            static_cast<Uint8>(pixelData[y][x] * 255.0f);  // Scale to 0-255
+        uint8_t color =
+            static_cast<uint8_t>(pixelData[y][x] * 255.0f);  // Scale to 0-255
         pixels[y * WIDTH + x] =
             SDL_MapRGBA(format, color, color, color, 255);  // Grayscale
       }
     }
 
-    SDL_UpdateTexture(texture, NULL, pixels, WIDTH * sizeof(Uint32));
-
-    SDL_RenderSetScale(renderer, 10, 10);
+    SDL_UpdateTexture(texture, NULL, pixels, WIDTH * sizeof(uint32_t));
 
     // Render the texture
     SDL_RenderClear(renderer);
     SDL_RenderCopy(renderer, texture, NULL, NULL);
+    static float angle = 0;
+    draw_arrow(renderer, 100, 100, 30, angle);
+    SDL_Delay(5);
+    angle += 0.01;
+
     SDL_RenderPresent(renderer);
   }
 
