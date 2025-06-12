b800ab57d1ace2d4778bf41abb149f4bae212183
Ali Jafari <ali81jafari81@gmail.com>
2025-06-13 00:01:12 +0330

    chore: removed unused constants

---
+++ inc/config.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <cmath>
+
+#define FPS_AVG_SIZE 10
+#define ARROW_HEAD_ANGLE M_PI / 8
+#define REPORT_FILE_NAME "report.log"
+#define ALLOW_LOG_ON_RELEASE true
+#define FPS_LOG_SPACER 1
